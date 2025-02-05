5cfda4224ebcb087dbc2008b19d8d314b8466114
unibox <unibox@duck.com>
2025-02-05 12:30:11 +0100

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     imageService: 'cloudflare',
   }),
+
+  integrations: [svelte()],
 });
